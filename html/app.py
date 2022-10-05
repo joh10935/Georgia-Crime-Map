dc2901b3d4802e8a0638bc69c798efa4349fd1bb
@johnmcmullan0819-JIKvRK <john.mcmullan0819@gmail.com>
2022-10-05 11:34:11 -0400

    Flask added to map

---
+++ html/app.py
@@ -0,0 +1,109 @@
+import numpy as np
+import datetime as dt
+# Import Alchemy
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+# Import Flask
+from flask import Flask, jsonify
+from sqlalchemy import text
+import pandas as pd
+from flask_cors import CORS, cross_origin
+import collections
+import json
+import psycopg2
+
+# Set up engine
+# engine = create_engine(f'postgresql://postgres:Techbobcats=0618@localhost/GACRIME')
+# Base = automap_base()
+# Base.prepare(engine, reflect=True)
+# conn = engine.connect()
+
+conn_string = "host='localhost' dbname='GACRIME' user='postgres' password='Techbobcats=0618'"
+conn = psycopg2.connect(conn_string)
+cursor = conn.cursor()
+
+# Create Flask
+app = Flask(__name__)
+CORS(app)
+
+
+
+# Different API routes:
+@app.route("/")
+def welcome():
+    """Homepage."""
+    """List all available routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"Crime Agency Data: /api/agency<br/>"
+        f"Median Income by County Data: /api/income<br/>"
+        f"Crime Data: /api/crime<br/>"
+    )
+
+
+@app.route('/api/agency')
+def agency():
+    # session = Session(engine)
+    x=[]
+    results = cursor.execute('SELECT public."Agency".agency_name, CAST(sum(public."Offense".cleared) as bigint), CAST(sum(public."Offense".actual) as bigint) FROM public."Agency" Left Join public."Offense" on public."Offense".ori = public."Agency".ori group by public."Agency".agency_name;')
+    rows = cursor.fetchall()
+    # results = pd.read_sql('SELECT * FROM public."Agency"', conn)
+    for row in rows:
+        d = collections.OrderedDict()
+        d['agency_name'] = row[0]
+        d['cleared'] = row[1]
+        d['actual'] = row[2]
+
+        x.append(d)
+    return jsonify(x)
+
+@app.route('/api/agencyLocation')
+def agencyLocation():
+    x=[]
+    results = cursor.execute('SELECT public."Agency".agency_name, public."Agency".Longitude, public."Agency".Latitude FROM public."Agency" where public."Agency".Latitude IS NOT NULL')
+    rows = cursor.fetchall()
+    # results = pd.read_sql('SELECT * FROM public."Agency"', conn)
+    for row in rows:
+        d = collections.OrderedDict()
+        d['agency_name'] = row[0]
+        d['Longitude'] = row[1]
+        d['Latitude'] = row[2]
+
+        x.append(d)
+    return jsonify(x)
+
+
+@app.route('/api/Income')
+def Income():
+    x=[]
+    results = cursor.execute('SELECT public."Income".countyid, public."Income".percapita_income, public."Income".data_year, public."County".county_name FROM public."Income" Left join public."County" on public."Income".countyid = public."County".countyid where public."Income".data_year = 2020')
+    rows = cursor.fetchall()
+    # results = pd.read_sql('SELECT * FROM public."Agency"', conn)
+    for row in rows:
+        d = collections.OrderedDict()
+        d['Percapita_Income'] = row[1]
+        d['County_Name'] = row[3]
+
+        x.append(d)
+    return jsonify(x)
+
+@app.route('/api/CountyCrimes')
+def CountyCrimes():
+    x=[]
+    results = cursor.execute('SELECT CAST(sum(public."Offense".cleared) as bigint), CAST(sum(public."Offense".actual) as bigint), public."Agency".county_name From public."Offense" left join public."Agency" on public."Offense".ori = public."Agency".ori group by public."Agency".county_name;')
+    rows = cursor.fetchall()
+    # results = pd.read_sql('SELECT * FROM public."Agency"', conn)
+    for row in rows:
+        d = collections.OrderedDict()
+        d['cleared'] = row[0]
+        d['actual'] = row[1]
+        d['county_name'] = row[2]
+
+        x.append(d)
+    return jsonify(x)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- html/static/js/map.js
+++ html/static/js/map.js
@@ -20,37 +20,51 @@ function createMap(county) {
     Departments: loclayer,
   };
 
-  // Legend Creation
-  var legend = L.control({
-    position: "bottomright",
-  });
 
-  legend.onAdd = function () {
-    var div = L.DomUtil.create("div", "info legend");
-
-    //County Legend
-    departmentIntervals = [0, 25, 75];
-
-    departmentColor = ["red", "yellow", "green"];
-    div.innerHTML = "<h3>Crime Solved Rate</h3>";
-    for (a = 0; a < departmentIntervals.length; a++) {
-      div.innerHTML += "<i style='background: " + departmentColor[a] + "'></i>" + departmentIntervals[a] +
-        (departmentIntervals[a + 1] ? " &ndash; " + departmentIntervals[a + 1] + " Percent" + " <br>": "+");
+  var legend = L.control(
+    {
+      position: "bottomright"
     }
+  )
 
-    div.innerHTML += "<br><i style='background: purple'></i>NaN";
+  legend.onAdd = function()
+  {
+      var div = L.DomUtil.create("div", "info legend");
 
-    //Department Legend
-    var intervals = [0, 37500, 55000, 70000];
+      departmentIntervals = [0, 25, 75]
+     
+      departmentColor = [
+        'red',
+        'yellow',
+        'green'
+      ]
+      div.innerHTML = '<h3>Crime Solved Rate</h3>'
+      for(a=0; a<departmentIntervals.length; a++)
+      {
+        div.innerHTML += "<i style='background: " + departmentColor[a] + "'></i>" 
+           + departmentIntervals[a] + (departmentIntervals[a+1]  ? ' &ndash; ' + departmentIntervals[a+1] + ' Percent' + ' <br>': '+')
+      }
+      
+      
+      div.innerHTML += "<br><i style='background: purple'></i>NaN"
 
-    var colors = ["#FF0000", "#ff4500", "#00FFFF", "#008000"];
-    div.innerHTML += "<h3>Median Household Income</h3>";
-    for (var i = 0; i < intervals.length; i++) {
-      div.innerHTML += "<i style='background: " + colors[i] + "'></i>" + "$" + intervals[i] +
-        (intervals[i + 1] ? " &ndash; " + intervals[i + 1] + " <br>" : "+");}
-    return div;
-  };
-  legend.addTo(myMap);
+      var intervals = [0, 37500, 55000, 70000];
+
+      var colors = [
+          '#FF0000',
+          '#ff4500',
+          '#00FFFF',
+          '#008000'
+      ]
+      div.innerHTML += '<h3>Median Household Income</h3>'
+      for(var i=0; i<intervals.length; i++)
+      {
+          div.innerHTML += "<i style='background: " + colors[i] + "'></i>" 
+          + '$' + intervals[i] + (intervals[i+1]  ? ' &ndash; ' + intervals[i+1] + ' <br>': '+')
+      }
+      return div;
+  }
+  legend.addTo(myMap)
 
   L.control
     .layers(baseMaps, overlayMaps, {
@@ -59,185 +73,200 @@ function createMap(county) {
     .addTo(myMap);
 }
 
-//Crime Data per Department
-d3.json("api/agency").then(function (crimes) {
-  {
-    crimedct = [];
 
-    for (i = 0; i < crimes.data.length; i++) {
-      loc = crimes[i].agency_name;
-      clear = crimes[i].cleared;
-      actual = crimes[i].actual;
-      percent = ((clear / actual) * 100).toFixed(2);
-      console.log(crimes)
-      //Set Marker Colors per Solve rate
-      var markerColor;
 
-      if (percent > 75)
-        markerColor = new L.Icon({
-          iconUrl: "img/marker-icon-2x-green.png",
-          shadowUrl: "img/marker-shadow.png",
-          iconSize: [25, 41],
-          iconAnchor: [12, 41],
-          popupAnchor: [1, -34],
-          shadowSize: [41, 41],
-        });
-      else if (percent > 25)
-        markerColor = new L.Icon({
-          iconUrl: "img/marker-icon-2x-yellow.png",
-          shadowUrl: "img/marker-shadow.png",
-          iconSize: [25, 41],
-          iconAnchor: [12, 41],
-          popupAnchor: [1, -34],
-          shadowSize: [41, 41],
-        });
-      else if (percent >= 0)
-        markerColor = new L.Icon({
-          iconUrl: "img/marker-icon-2x-red.png",
-          shadowUrl: "img/marker-shadow.png",
-          iconSize: [25, 41],
-          iconAnchor: [12, 41],
-          popupAnchor: [1, -34],
-          shadowSize: [41, 41],
-        });
-      else
-        markerColor = new L.Icon({
-          iconUrl: "img/marker-icon-2x-violet.png",
-          shadowUrl: "img/marker-shadow.png",
-          iconSize: [25, 41],
-          iconAnchor: [12, 41],
-          popupAnchor: [1, -34],
-          shadowSize: [41, 41],
-        });
-
-      x = { loc, clear, actual, percent, markerColor };
-
-      crimedct.push(x);
-    }
-  }
-
-  //Department Locations
-  d3.json("../data/Ga_Department_Locations.json").then(function (countydata) {
+d3.json('http://127.0.0.1:5000/api/agency').then(
+  function(crimes){
     {
-      locations = [];
+      crimedct = []
 
-      purpleMarker = new L.Icon({
-        iconUrl: "img/marker-icon-2x-violet.png",
-        shadowUrl: "img/marker-shadow.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41],
-      });
-
-      for (i = 0; i < countydata.length; i++) {
-        lat = countydata[i].Latitude;
-        lng = countydata[i].Longitude;
-        Income = countydata[i]["2020 Income per Capita"];
-
-        if (_.findIndex(crimedct, { loc: countydata[i]["Agency Name"] }) >= 0)
-          locArea = L.marker([lat, lng], {icon: crimedct[_.findIndex(crimedct, { loc: countydata[i]["Agency Name"] })].markerColor,}).bindPopup(
-            `<h2> ${countydata[i]["Agency Name"]}</h2><hr> <p>Total crimes: ${crimedct[_.findIndex(crimedct, { loc: countydata[i]["Agency Name"] })].actual
-            } </p>Solved Crimes: ${crimedct[_.findIndex(crimedct, { loc: countydata[i]["Agency Name"] })].clear}</p><p> Percent Solved: ${
-              crimedct[_.findIndex(crimedct, { loc: countydata[i]["Agency Name"] })].percent}%`);
-        else
-          locArea = L.marker([lat, lng], { icon: purpleMarker }).bindPopup(`<h2> ${countydata[i]["Agency Name"]}</h2>`);
-
-        locations.push(locArea);
-      }
-
-      loclayer = L.layerGroup(locations);
-    }
-
-    //County Income Data
-    d3.json("../data/ID_capita.json").then(function (income) {
-      ctycolor = [];
-      combined = [];
-
-      for (i = 0; i < income.length; i++) {
-        var countycolor;
-
-        if (income[i]["2020 Income per Capita"] > 70000)
-          countycolor = "#008000";
-        else if (income[i]["2020 Income per Capita"] > 55000)
-          countycolor = "#00FFFF";
-        else if (income[i]["2020 Income per Capita"] > 37500)
-          countycolor = "#ff4500";
-        else countycolor = "#FF0000";
-
-        var cty = income[i]["County"];
-        var inc = income[i]["2020 Income per Capita"];
-        cty = _.toUpper(cty);
-        x = { cty, countycolor, inc };
-        combined.push(x);
-      }
-
-      //County Crime data
-      d3.json("../data/crimes_county.json").then(function (crimelocation) {
-        cldict = [];
-
-        for (i = 0; i < crimelocation.data.length; i++) {
-          countloc = crimelocation.data[i].county_name;
-          countclear = crimelocation.data[i].cleared;
-          countactual = crimelocation.data[i].actual;
-          countpercent = ((countclear / countactual) * 100).toFixed(2);
-          z = { countloc, countclear, countactual, countpercent };
-
-          cldict.push(z);
-        }
-
-        //County layout
-        d3.json("../data/GA-13-georgia-counties.json").then(function (data) {
-          var geojson = topojson.feature(data, data.objects.cb_2015_georgia_county_20m).features;
-
-          var county = L.geoJson(geojson, {
-            style: function (Feature) {
-              if (_.findIndex(combined, {cty: _.toUpper(Feature.properties.NAME) + " COUNTY",}) >= 0) {
-                return {
-                  color: "black",
-                  fillColor:combined[_.findIndex(combined, {cty: _.toUpper(Feature.properties.NAME) + " COUNTY",})].countycolor,
-                  fillOpacity: 0.3,
-                  weight: 2,
-                };
-              } else
-                return {
-                  color: "black",
-                  fillColor: "white",
-                  fillOpacity: 0.3,
-                  weight: 2,
-                };
-            },
-            onEachFeature: function (Feature, layer) {
-              layer.on({
-                mouseover: function (event) {
-                  layer = event.target;
-                  layer.setStyle({fillOpacity: 0.85,});
-                },
-
-                mouseout: function (event) {
-                  layer = event.target;
-                  layer.setStyle({fillOpacity: 0.3,});
-                },
-
-                click: function (event) {
-                  layer = event.target;
-                  //myMap.fitBounds(layer.getBounds());
-                },
-              });
+      for(i = 0; i < crimes.length; i++){
+        loc = crimes[i].agency_name
+        clear = crimes[i].cleared
+        actual = crimes[i].actual
+        percent = ((clear/actual)*100).toFixed(2)
+        var markerColor;
 
 
-              if (_.findIndex(cldict, {countloc: _.toUpper(Feature.properties.NAME),}) >= 0)
-                layer.bindPopup(`<h2>${Feature.properties.NAME} County </h2> <hr> <p>2020 Median Income: $${combined[_.findIndex(combined, {cty: _.toUpper(Feature.properties.NAME) + " COUNTY",})].inc}</p> 
-                <p>Total Crimes: ${cldict[_.findIndex(cldict, {countloc: _.toUpper(Feature.properties.NAME),})].countactual}</p> <p>Solved Crimes: ${cldict[_.findIndex(cldict, {countloc: _.toUpper(Feature.properties.NAME),})].countclear}</p>
-                <p>Percent of Crimes Solved: ${cldict[_.findIndex(cldict, {countloc: _.toUpper(Feature.properties.NAME),})].countpercent}%`);
-              else 
-                layer.bindPopup(`${Feature.properties.NAME} County <hr> `);
-            },
+        if(percent > 75)
+          markerColor =  new L.Icon({
+            iconUrl: 'img/marker-icon-2x-green.png',
+            shadowUrl: 'img/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+          });
+        
+        else if(percent > 25)
+          markerColor =  new L.Icon({
+            iconUrl: 'img/marker-icon-2x-yellow.png',
+            shadowUrl: 'img/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
           });
 
-          createMap(county);
-        });
+        else if(percent >= 0)
+          markerColor =  new L.Icon({
+            iconUrl: 'img/marker-icon-2x-red.png',
+            shadowUrl: 'img/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+          });
+
+        else 
+          markerColor =  new L.Icon({
+            iconUrl: 'img/marker-icon-2x-violet.png',
+            shadowUrl: 'img/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+          });
+
+        x = {loc, clear, actual, percent, markerColor}
+          
+
+        crimedct.push(x)
+
+      }
+    }
+
+d3.json("http://127.0.0.1:5000/api/agencyLocation").then(function (countydata) {
+  {
+    locations = [];
+    purpleMarker =  new L.Icon({
+      iconUrl: 'img/marker-icon-2x-violet.png',
+      shadowUrl: 'img/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]})
+
+    for (i = 0; i < countydata.length; i++) {
+      lat = countydata[i].Latitude;
+      lng = countydata[i].Longitude;
+      
+      if (_.findIndex(crimedct, {loc: (countydata[i].agency_name)}) >= 0)
+        locArea = L.marker([lat, lng], {icon: crimedct[_.findIndex(crimedct, {loc: (countydata[i].agency_name)})].markerColor}).bindPopup(
+          `<h2> ${countydata[i].agency_name}</h2><hr> <p>Total crimes: ${crimedct[_.findIndex(crimedct, {loc: (countydata[i].agency_name)})].actual} </p>
+          Solved Crimes: ${crimedct[_.findIndex(crimedct, {loc: (countydata[i].agency_name)})].clear}</p><p> Percent Solved: ${crimedct[_.findIndex(crimedct, {loc: (countydata[i].agency_name)})].percent}%`);
+      else
+        locArea = L.marker([lat, lng], {icon: purpleMarker}).bindPopup(
+          `<h2> ${countydata[i].agency_name}</h2>`);
+
+      locations.push(locArea);
+      
+
+    }
+
+    loclayer = L.layerGroup(locations);
+  }
+
+  d3.json("http://127.0.0.1:5000/api/Income").then(function (income) {
+
+    combined = [];
+
+    for (i = 0; i < income.length; i++) {
+      var countycolor;
+
+      if (income[i].Percapita_Income > 70000) 
+        countycolor = "#008000";
+      else if (income[i].Percapita_Income > 55000)
+        countycolor = "#00FFFF";
+      else if (income[i].Percapita_Income > 37500)
+        countycolor = "#ff4500";
+      else 
+        countycolor = "#FF0000";
+
+      var cty = income[i].County_Name;
+
+      cty = _.toUpper(cty);
+      x = { cty, countycolor };
+      combined.push(x);
+    }
+
+    d3.json('http://127.0.0.1:5000/api/CountyCrimes').then(
+      function(crimelocation){
+
+        cldict = []
+
+        for (i = 0; i < crimelocation.length; i++){
+          countloc = crimelocation[i].county_name
+          countclear = crimelocation[i].cleared
+          countactual = crimelocation[i].actual
+          countpercent = ((countclear/countactual)*100).toFixed(2)
+          z = {countloc, countclear, countactual, countpercent}
+
+          cldict.push(z)
+        
+        }
+
+
+
+    d3.json("../data/GA-13-georgia-counties.json").then(function (data) {
+      var geojson = topojson.feature(
+        data,
+        data.objects.cb_2015_georgia_county_20m
+      ).features;
+
+      
+      var county = L.geoJson(geojson, {
+        style: function (Feature) {
+          if (_.findIndex(combined, {cty: _.toUpper(Feature.properties.NAME)}) >= 0) {
+            return {
+              color: "black",
+              fillColor:
+                combined[_.findIndex(combined, {cty: _.toUpper(Feature.properties.NAME)})].countycolor,
+              fillOpacity: 0.3,
+              weight: 2,
+            };
+          } else
+            return {
+              color: "black",
+              fillColor: "white",
+              fillOpacity: 0.3,
+              weight: 2,
+            };
+        },
+        onEachFeature: function (Feature, layer) {
+          layer.on({
+            mouseover: function (event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.85,
+              });
+            },
+
+            mouseout: function (event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.3,
+              });
+            },
+
+            click: function (event) {
+              layer = event.target;
+              //myMap.fitBounds(layer.getBounds());
+            },
+          });
+       
+          if (_.findIndex(cldict, {countloc: _.toUpper(Feature.properties.NAME),}) >= 0)
+            layer.bindPopup(`<h2>${Feature.properties.NAME} County </h2> <hr> <p>Total Crimes: ${(cldict[(_.findIndex(cldict, {countloc: _.toUpper(Feature.properties.NAME),}))].countactual)}</p>
+            <p>Solved Crimes: ${(cldict[(_.findIndex(cldict, {countloc: _.toUpper(Feature.properties.NAME),}))].countclear)}</p><p>Percent of Crimes Solved: ${(cldict[(_.findIndex(cldict, {countloc: _.toUpper(Feature.properties.NAME),}))].countpercent)}%`);
+          else
+            layer.bindPopup(`${Feature.properties.NAME} County <hr> `);
+          
+        },
       });
+
+      createMap(county);
     });
   });
 });
+});
+})
